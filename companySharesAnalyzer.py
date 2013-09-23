ca566119342d42a9fe07ae0057ed1a33f12ddd75
excelsior43 <sonu.yasir@gmail.com>
2013-09-23 11:31:11 +0300

    Update companySharesAnalyzer.py

--- companySharesAnalyzer.py
+++ companySharesAnalyzer.py
@@ -1,7 +1,7 @@
 from companyShareAnalyzerBean import CompanyDataBean
 class CompanyShareAnalyzer:
     ''' This is the main analysis class that does the overall analysis '''
-    def __init__(self,path):
+    def __init__(self,path='shareData.csv'):
         self.__path__=path
         self.result_=None
     def readFile(self):
@@ -13,13 +13,13 @@ class CompanyShareAnalyzer:
         try:
             with open(self.__path__, 'r') as f:
                 for ln in f:
-                    ln = ln.split(',')[:-1] # Removing the last ','
+                    ln = ln.split(',') # Removing the last ','
                     if self.result_ == None: # first time to read all company names
                         self.result_= [ CompanyDataBean(x,0,None,0) for x in ln[2:]]
                         continue
                     yield (ln[:2],ln[2:])
 	except IOError, err:
-            print "Please Select a proper file name Found exception : %r" %(err)
+            print "Exception : %r" %(err)
             
     def getResultSharePriceAnalysis(self):
         '''
@@ -29,4 +29,6 @@ class CompanyShareAnalyzer:
             for i, val in enumerate(shareValue):
                if self.result_[i] >  val:
                    self.result_[i]=CompanyDataBean(self.result_[i].name,val,period[1],period[0])
+        for i in self.result_ :
+            print i
         return self.result_    
